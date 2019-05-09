19966d632dc1556b4b6c224e6e9b767aa0a2f342
99Andrade <delmiandrade1023@gmail.com>
2019-05-09 08:47:28 -0600

    Fragments

---
+++ app/src/main/java/com/example/fragmentos/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.fragmentos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var btnFragmento: Button
+
+    private lateinit var fragment1: FragmentBlank
+    private lateinit var fragment2: BlankFragment2
+    private lateinit var fragment3: BlankFragment3
+
+
+    var FragmentoTurno = 0
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnFragmento = findViewById(R.id.btnFragmento)
+
+        fragment1 = FragmentBlank()
+        fragment2 = BlankFragment2()
+        fragment3 = BlankFragment3()
+
+        btnFragmento.setOnClickListener {
+            IntercalarFragmentos(it)
+        }
+    }
+
+
+
+    private fun IntercalarFragmentos(view: View) {
+        val manager = supportFragmentManager
+        val transaction = manager.beginTransaction()
+
+        if (FragmentoTurno == 0) {
+            //Instalar aqui el fragmento 1
+            transaction.replace(R.id.reemplazarAqui, fragment1)
+            transaction.addToBackStack(null)
+            transaction.commit()
+            FragmentoTurno++
+
+        } else if (FragmentoTurno == 1) {
+            //instalar aqui el fragmento 2
+            transaction.replace(R.id.reemplazarAqui, fragment2)
+            transaction.addToBackStack(null)
+            transaction.commit()
+            FragmentoTurno++
+
+        } else if (FragmentoTurno == 2) {
+            //instalar aqui el fragmento 3
+            transaction.replace(R.id.reemplazarAqui, fragment3)
+            transaction.addToBackStack(null)
+            transaction.commit()
+            FragmentoTurno = 0
+
+
+        }
+    }
+}
